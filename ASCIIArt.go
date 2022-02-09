46424376d8e3f04d67f750dc71a1dc363ca8005f
Nathaniel A <mineandis@gmail.com>
2022-02-08 19:21:01 -0600

    hopefully the playground runs this correctly

--- ASCIIArt.go
+++ ASCIIArt.go
@@ -1,1 +1,55 @@
+package main
 
+import (
+	f "fmt"
+)
+
+func main() {
+	f.Println("        .--'''''''''--.")
+	f.Println("     .'      .---.      '.")
+	f.Println("    /    .-----------.    \\")
+	/*Comment break to not spoil anything, run the code yourself, dummy!
+	f.Println("    /\( )/\"
+	f.Println("  _.-'  /   \  `-._ \___/ \___/")
+	*/
+	f.Println("   /        .-----.        \\")
+	f.Println("   |       .-.   .-.       |")
+	f.Println("   |      /   \\ /   \\      |")
+	/*Comment break to not spoil anything, run the code yourself, dummy!
+	f.Println(4123432432123jfasekfsdfdsaklfj3eqp9r9weqhj 98ioqrfhsdnfbdnbsfjindsailkfsdjkf s GODDSADas"
+	f.Println(          | |-----| |
+	       .`/  |     | |/`.)
+	*/
+	f.Println("    \\    | .-. | .-. |    /")
+	f.Println("     '-._| | | | | | |_.-'")
+	f.Println("         | '-' | '-' |")
+	/*Comment break to not spoil anything, run the code yourself, dummy!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	*/
+	f.Println("          \\___/ \\___/")
+	f.Println("       _.-'  /   \\  `-._")
+	f.Println("     .' _.--|     |--._ '.")
+	/*Comment break to not spoil anything, run the code yourself, dummy!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	Want a break from the ads? If you tap now to watch a short video, you'll receive 30 minutes of ad-free music!
+	*/
+	f.Println("            |     |")
+	f.Println("            '.___.'")
+}
