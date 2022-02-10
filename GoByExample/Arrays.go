9868eda3941e993c7eb2e96208928fc7d41c28ba
Nathaniel A <mineandis@gmail.com>
2022-02-10 14:33:21 -0600

    Added Documentation

--- GoByExample/Arrays.go
+++ GoByExample/Arrays.go
@@ -3,19 +3,25 @@ package main
 import "fmt"
 
 func main() {
-
+	//a is declared as an array with 5 integers. Since those integers haven't been declared, they are defaulted to 0.
 	var a [5]int
 	fmt.Println("emp:", a)
 
+	//the fifth integer in array "a" is set to 100. (Note: it counts starting from zero)
 	a[4] = 100
-	fmt.Println("set:", a)
-	fmt.Println("get:", a[4])
+	fmt.Println("set:", a)    //Get the entire array printed
+	fmt.Println("get:", a[4]) //Get only the fifth integer printed
 
+	//Print the length of *how many* integers are in the array.
 	fmt.Println("len:", len(a))
 
+	//Shorthand "b" to be a five-length integer array with the integers set to 1, 2, 3, 4, 5.
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
+	//Create two arrays with 3 integers in the first array, and 3 integers in the third array.
+	//Using a for loop allows you to access the values of each value of the arrays.
+	//The outer array is 2 arrays in length. The inner arrays are 3 integers in length each.
 	var twoD [2][3]int
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 3; j++ {
--- GoByExample/Slices.go
+++ GoByExample/Slices.go
@@ -3,38 +3,47 @@ package main
 import "fmt"
 
 func main() {
-
+	//Make slice "s" with 3 empty string values.
 	s := make([]string, 3)
 	fmt.Println("emp:", s)
 
-	s[0] = "a"
-	s[1] = "b"
-	s[2] = "c"
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
+	s[0] = "a"                //Set the first value of slice "s" to "a"
+	s[1] = "b"                //Set the second value of slice "s" to "b"
+	s[2] = "c"                //Set the third value of slice "s" to "c"
+	fmt.Println("set:", s)    //Print all the values of s
+	fmt.Println("get:", s[2]) //Print the third value of s
 
-	fmt.Println("len:", len(s))
+	fmt.Println("len:", len(s)) //Print the length of s
 
-	s = append(s, "d")
-	s = append(s, "e", "f")
+	//Using append allows you to add another value to a slice. Note this will not work with an array.
+	s = append(s, "d")      //Adding one value here.
+	s = append(s, "e", "f") //Adding two values here.
 	fmt.Println("apd:", s)
 
+	//Making a new slice which has the length of slice s.
 	c := make([]string, len(s))
-	copy(c, s)
+	copy(c, s) //Copying the values of s into the values of c [Recipient, Original]
 	fmt.Println("cpy:", c)
 
+	//A slice operator which gets the values of the third value up to the fifth value.
+	//Note that the operators' inclusion involves an [including, excluding] formula.
 	l := s[2:5]
 	fmt.Println("sl1:", l)
 
+	//A slice operator which gets all values of s, excluding the sixth (and any beyond) digit.
 	l = s[:5]
 	fmt.Println("sl2:", l)
 
+	//Starts the slice operator after and including the third value.
 	l = s[2:]
 	fmt.Println("sl3:", l)
 
+	//Declare and initialize a slice (using an empty []) with the values "g", "h", and "i".
 	t := []string{"g", "h", "i"}
 	fmt.Println("dcl:", t)
 
+	//Create two integer slices with a maximum length of 3.
+	//The slices cannot exceed a length of 3, but can have less than 3 values.
 	twoD := make([][]int, 3)
 	for i := 0; i < 3; i++ {
 		innerLen := i + 1
