74daf24bb63a7428425c962b5ae1427b1528dfe2
Nathaniel A <mineandis@gmail.com>
2022-02-10 12:52:01 -0600

    Added Documentation

--- GoByExample/Switch.go
+++ GoByExample/Switch.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-
+	//i is equal to 2. First, print out "Write 2 as" and see which case it fits. In the case i equals 2, add "two" to the print statement.
 	i := 2
 	fmt.Print("Write ", i, " as ")
 	switch i {
@@ -17,14 +17,14 @@ func main() {
 	case 3:
 		fmt.Println("three")
 	}
-
+	//Take today's day. If it is a Saturday or Sunday, print "It's the weekend". Otherwise, default to printing "It's a weekday".
 	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
 		fmt.Println("It's the weekend")
 	default:
 		fmt.Println("It's a weekday")
 	}
-
+	//Take the current time. If the time is before 12 (12:00 PM), then print "It's before noon". Otherwise, default to printing "It's after noon".
 	t := time.Now()
 	switch {
 	case t.Hour() < 12:
@@ -32,7 +32,12 @@ func main() {
 	default:
 		fmt.Println("It's after noon")
 	}
-
+	/*
+		Interfaces are a set of types, in this case, bool and int.
+		whatAmI is defined below the function with different types.
+		"t" takes each type one at a time and compares it with the cases.
+		If "t" receives a type that is not a bool or an int, it'll print the default case, which will print "don't know type 'type' (in this case, a string)"
+	*/
 	whatAmI := func(i interface{}) {
 		switch t := i.(type) {
 		case bool:
